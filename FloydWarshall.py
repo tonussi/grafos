01c861c9edec51f63aaea4cd0dd7176338992dc1
tonussi <lptonussi@gmail.com>
2016-04-15 18:18:05 -0300

    Add find routes

--- FloydWarshall.py
+++ FloydWarshall.py
@@ -1,14 +1,32 @@
-import Chronometer
+from Chronometer import timeit
 
-class FloydWarshall(object):
+class FloydWarshall:
     '''
     This class implements the function to extract the matrices
     of Routes and matrices of Adjacency from a given matrix that
     represents a Graph
     '''
 
-    def __init__(self, MatrixGraph):
-        '''
-        Use @timeit in a method to get the time
-        '''
-        self.MatrixGraph = MatrixGraph
+    @timeit
+    def floydWarshallWithPathReconstruction(self, dist, next, V):
+        for (u,v) in V:
+            dist[u][v] = self.weight(u,v)
+            next[u][v] = v
+        for k in range(1,len(V)):
+            for i in range (1, len(V)):
+                for j in range(1, len(V)):
+                    if dist[i][k] + dist[k][j] < dist[i][j]:
+                        dist[i][j] = dist[i][k] + dist[k][j]
+                        next[i][j] = next[i][k]
+
+    def weight(self, u, v):
+        pass
+
+    def path(self, next, u, v):
+        if next[u][v] is None:
+            return []
+        path = [u]
+        while u is not v:
+            u = next[u][v]
+            path.append(u)
+            return path
