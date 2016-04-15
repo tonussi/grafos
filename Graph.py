3268f6a391f758fdca542ffbd75b0ad5cf17bea6
tonussi <lptonussi@gmail.com>
2016-04-15 18:18:47 -0300

    Add findBestWayToMedicalCenter

--- Graph.py
+++ Graph.py
@@ -1,13 +1,13 @@
-from Floyd import FloydWarshall
+from FloydWarshall import FloydWarshall
+from time import sleep
 
-class Graph(object):
+class Graph:
 	"""<h1>Simple Graph Class</h1>
 	<p>This class implement a Graph
 	structure with basic funcions.</p>"""
 
-	def __init__(self, arg={}, name=''):
-		super(Graph, self).__init__()
-		self.graph = arg
+	def __init__(self, graphdict={}, name=''):
+		self.graph = graphdict
 		self.name = name
 
 	def name(self, name):
@@ -30,32 +30,8 @@ class Graph(object):
 	def getEdge(self, v):
 		return self.graph[v]
 
-class Hospital(Graph, FloydWarshall):
-	"""<h1>Simple Graph Class Impl of the Medical Center's problem</h1>
-	<p>This class implement a Graph for the Medical Center's problem
-	Its structured with basic functions and extends FloydWarshall actions.</p>"""
-
-	def __init__(self, arg={}, name=''):
-		super(Graph, self).__init__()
-		self.graph = arg
-		self.name = name
-
-	def name(self, name):
-		self.name = name
-
-	def addEdge(self, v):
-		self.graph[v] = []
-	
-	def addEdges(self, edges):
-		for e in edges:
-			self.graph[e]
-
-	def removeEdge(self, v):
-		if v in self.graph: 
-			del self.graph[v]
-
-	def addAdjacency(self, v, a):
-		self.graph[v].append(a)
-
-	def getEdge(self, v):
-		return self.graph[v]
+	def findBestWayToMedicalCenterRoute(self):
+		# TODO: Usar funcionalidades do floyd warshall
+		# para construir esse metodo de achar o melhor
+		# caminho
+		sleep(2)
