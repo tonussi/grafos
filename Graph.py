89278ab4cbe71b80f157914737d0d1f69f12f38b
tonussi <lptonussi@gmail.com>
2016-04-15 15:19:24 -0300

    Add Graph

--- Graph.py
+++ Graph.py
@@ -1,7 +1,39 @@
+from Floyd import FloydWarshall
+
 class Graph(object):
 	"""<h1>Simple Graph Class</h1>
 	<p>This class implement a Graph
 	structure with basic funcions.</p>"""
+
+	def __init__(self, arg={}, name=''):
+		super(Graph, self).__init__()
+		self.graph = arg
+		self.name = name
+
+	def name(self, name):
+		self.name = name
+
+	def addEdge(self, v):
+		self.graph[v] = []
+	
+	def addEdges(self, edges):
+		for e in edges:
+			self.graph[e]
+
+	def removeEdge(self, v):
+		if v in self.graph: 
+			del self.graph[v]
+
+	def addAdjacency(self, v, a):
+		self.graph[v].append(a)
+
+	def getEdge(self, v):
+		return self.graph[v]
+
+class Hospital(Graph, FloydWarshall):
+	"""<h1>Simple Graph Class Impl of the Medical Center's problem</h1>
+	<p>This class implement a Graph for the Medical Center's problem
+	Its structured with basic functions and extends FloydWarshall actions.</p>"""
 
 	def __init__(self, arg={}, name=''):
 		super(Graph, self).__init__()
