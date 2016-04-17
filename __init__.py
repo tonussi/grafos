9f5eae33744af0cc9a110e0543a04afb190e3ff0
tonussi <lptonussi@gmail.com>
2016-04-17 18:26:54 -0300

    Add simple text menu

--- __init__.py
+++ __init__.py
@@ -1,25 +1,46 @@
 # encoding:utf-8
+
 from Graph import Graph
 from FileReader import FileReader
 from Hospital import Hospital
 from RandomGraphGenerator import RandomGraphGenerator
-import sys, getopt, os
 from FloydWarshall import FloydWarshall
-import threading, random
 
-# v1 -> v2 custo
-#  1 ->  2  1000
-#  2 ->  4  1000
-#  3 ->  1  1000
-#  4 ->  2  1000
+import sys
+import getopt
+import os
+import threading
+import random
 
-class SamuThread(threading.Thread):
+"""
+You can choose -e or --export to build the dat directory
+this will populate the dat directory with graphs
+You also can start the calculation of a graph at time
+just input in your terminal the following line:
+python __init__.py -i dat/<name_of_file>
+and them fo to the /results directory at the root of this
+software package to pick up your results.
+note: this program already build regular graphs with random
+costs, and this program also makes use of an good algorithm to
+to build random regular graphs, see RandomGraphGenerator.py for
+more information. Another thing to account is that the program
+divide the efforts to find the best path into to running threads.
+each thread called Samu Slave will calculate the best path for the
+ambulance to arrive at his Medical Center. What I mean is that a
+ambulance A is related to medical center A and not to medical center B
+wich is related to ambulance B.\n"
+"""
+
+
+class SamuSlave(threading.Thread):
+
     def __init__(self, hospital, threadID, name, routes):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
         self.routes = routes
         self.hospital = hospital
+
     def run(self):
         threadLock.acquire()
         self.hospital.findBestWayToMedicalCenterRoute()
@@ -28,51 +49,91 @@ class SamuThread(threading.Thread):
 threadLock = threading.Lock()
 threads = []
 
+
+def exportnewRegularGraphsToDat(edgesArray=[10, 20, 50, 100, 500]):
+    """
+    This function exports to files 5 or more regular graph without crossing edges
+    or also parallel edges.
+
+    There will be files in dat directory with at least the number of nodes passed
+    as parameter divided by 2 in edges.
+    """
+    graph_generator = RandomGraphGenerator()
+
+    for i in edgesArray:
+        set_of_edges = graph_generator.random_regular_graph(1, i)
+        dict_of_edges = graph_generator.convertSetToOrderedDict(set_of_edges)
+        FileReader.writef(
+            'dat' +
+            os.path.sep +
+            'results_' +
+            str(i) +
+            '_nodes' +
+            '.dat',
+            'edges=' +
+            str(dict_of_edges))
+
+def getDatDirectory():
+    return FileReader.listGraphsInDirectory('dat')
+
+
+def startThreadings():
+    hospitalA = Hospital()
+    hospitalB = Hospital()
+
+    # Create new threads
+    thread1 = SamuSlave(hospitalA, 0x1, "Operador Samu 1", [1, 2, 3])
+    thread2 = SamuSlave(hospitalB, 0x2, "Operador Samu 2", [3, 2, 1])
+
+    # Start new Threads
+    thread1.start()
+    thread2.start()
+
+    # Add threads to thread list
+    threads.append(thread1)
+    threads.append(thread2)
+
+    # Wait for all threads to complete
+    for t in threads:
+        t.join()
+
+
 def main(argv):
-	graphs_directory = 'dat'
-	graphs_list = []
+    edges = {}
 
-	graph_generator = RandomGraphGenerator()
+    try:
+        opts, args = getopt.getopt(argv, "h|i|e")
+    except getopt.GetoptError:
+        print("(1) python __init__.py -i <DAT_FILE_INPUT> (find best path)")
+        print("(2) python __init__.py -e (export)")
+        print("(3) python __init__.py -h (help)")
+        raise Warning("warning: did you forget parameters?")
+    for opt, arg in opts:
+        if opt == '-help':
+            print(
+                "(1) python __init__.py -i <DAT_FILE_INPUT> (find best path)")
+            print("(2) python __init__.py -e (export)")
+            print("(3) python __init__.py -h (help)")
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            if os.path.exists(arg):
+                edges = FileReader.readFile(arg)
+                print(edges)
+        elif opt in ("-e", "--export"):
+            if not os.path.exists('dat'):
+                try:
+                    os.makedirs('dat')
+                except IOError:
+                    raise Exception("it was impossible to create the given directory name")
 
-	for i in [10, 20, 25, 50, 100, 500]:
-		set_of_edges = graph_generator.random_regular_graph(2, i)
-		dict_of_edges = graph_generator.convertSetToDict(set_of_edges)
-		FileReader.writef('dat' + os.path.sep + 'results_' + str(i) + '_nodes' + '.dat', 'listOFGraphs=' + str(dict_of_edges))
+            exportnewRegularGraphsToDat()
+            print('visit dat directory to see your graphs')
 
-	listOFGraphs = FileReader.listGraphsInDirectory(graphs_directory)
-
-	for p in listOFGraphs:
-		graphs_list.append(FileReader.readFile(p))
-	
-	for g in graphs_list:
-		print g
-
-	hospitalA = Hospital()
-	hospitalB = Hospital()
-
-	# Create new threads
-	thread1 = SamuThread(hospitalA, 0x1, "Operador Samu 1", [1, 2, 3])
-	thread2 = SamuThread(hospitalB, 0x2, "Operador Samu 2", [3, 2, 1])
-
-	# Start new Threads
-	thread1.start()
-	thread2.start()
-
-	# Add threads to thread list
-	threads.append(thread1)
-	threads.append(thread2)
-
-	# Wait for all threads to complete
-	for t in threads:
-		t.join()
-
-	# graph = Graph({'hospitalA': [1, 1, 3]})
-	# graph.addEdge('hospitalB')
-	# graph.addAdjacency('hospitalB', 2)
-	# graph.addAdjacency('hospitalB', 2)
-	# graph.addAdjacency('hospitalB', 4)
-	# print ('hospitalB: ', graph.getEdge('hospitalB'))
-	# print ('hospitalA: ', graph.getEdge('hospitalA'))
+            if not os.path.exists('results'):
+                try:
+                    os.makedirs('results')
+                except IOError:
+                    raise Exception("it was impossible to create the given directory name")
 
 if __name__ == '__main__':
-   main(sys.argv[1:])
+    main(sys.argv[1:])
