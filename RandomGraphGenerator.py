d80d8cc1288cbfc4b3f1eadd414ea4d7e2ba1f41
tonussi <lptonussi@gmail.com>
2016-04-17 18:26:09 -0300

    Add ordered by keys converter

--- RandomGraphGenerator.py
+++ RandomGraphGenerator.py
@@ -1,10 +1,16 @@
-import os, random, time
-from collections import defaultdict
 from Graph import Graph
 from Chronometer import timeit
 
+import os
+import random
+import time
+
+from collections import defaultdict
+from collections import OrderedDict
+
+
 class RandomGraphGenerator(object):
-    
+
     def __init__(self, seed=None):
         if seed is not None:
             random.seed(seed)
@@ -13,10 +19,10 @@ class RandomGraphGenerator(object):
 
     @timeit
     def random_regular_graph(self, d, n, seed=None):
-        """Returns a random `d`-regular graph on `n` nodes.
-    
+        """Returns a random `d`-regular graph on `n` nodex.
+
         The resulting graph has no self-loops or parallel edges.
-    
+
         Parameters
         ----------
         d : int
@@ -25,31 +31,31 @@ class RandomGraphGenerator(object):
           The number of nodes. The value of :math:`n * d` must be even.
         seed : hashable object
             The seed for random number generator.
-    
+
         Notes
         -----
         The nodes are numbered from `0` to `n - 1`.
-    
-        Kim and Vu's paper [2]_ shows that this algorithm samples in an
-        asymptotically uniform way from the space of random graphs when
-        `d = O(n^{1 / 3 - \epsilon})`.
-    
+
+        Kim et al, 2003 shows that this algorithm have
+        asymptotically complexity of aproximately O(n^{1/3})
+
         Raises
         ------
-    
+
         Warning:
             If :math:`n * d` is odd or `d` is greater than or equal to `n`.
-    
+
         References
         ----------
         .. [1] A. Steger and N. Wormald,
            Generating random regular graphs quickly,
            Probability and Computing 8 (1999), 377-396, 1999.
            http://citeseer.ist.psu.edu/steger99generating.html
-    
+
         .. [2] Jeong Han Kim and Van H. Vu,
            Generating random regular graphs,
-           Proceedings of the thirty-fifth ACM symposium on Theory of computing,
+           Proceedings of the thirty-fifth ACM
+           symposium on Theory of computing,
            San Diego, CA, USA, pp 213--222, 2003.
            http://portal.acm.org/citation.cfm?id=780542.780576
         """
@@ -63,8 +69,8 @@ class RandomGraphGenerator(object):
             return
 
         def _suitable(edges, potential_edges):
-        # Helper subroutine to check if there are suitable edges remaining
-        # If False, the generation of the graph has failed
+            # Helper subroutine to check if there are suitable edges remaining
+            # If False, the generation of the graph has failed
             if not potential_edges:
                 return True
             for s1 in potential_edges:
@@ -80,34 +86,38 @@ class RandomGraphGenerator(object):
                     if (s1, s2) not in edges:
                         return True
             return False
-    
+
         def _try_creation():
             # Attempt to create an edge set
-    
+
             edges = set()
             stubs = list(range(n)) * d
-    
+
             while stubs:
                 potential_edges = defaultdict(lambda: 0)
                 random.shuffle(stubs)
                 stubiter = iter(stubs)
-                for s1, s2, s3 in zip(stubiter, stubiter, stubiter):
+
+                for s1, s2 in zip(stubiter, stubiter):
+
                     if s1 > s2:
                         s1, s2 = s2, s1
-                    if s1 != s2 and ((s1, s2, s3) not in edges):
+                    if s1 != s2 and ((s1, s2) not in edges):
 
                         # adiciona um vertice com custo aleatorio
-                        edges.add((s1, s2, s3 << random.randint(7, 11)))
+                        edges.add(
+                            (s1, s2, abs((s1 - s2) << random.randint(7, 11))))
 
                     else:
                         potential_edges[s1] += 1
                         potential_edges[s2] += 1
-    
+
                 if not _suitable(edges, potential_edges):
-                    return None # failed to find suitable edge set
-    
+                    return None  # failed to find suitable edge set
+
                 stubs = [node for node, potential in potential_edges.items()
                          for _ in range(potential)]
+
             return edges
 
         # Even though a suitable edge set exists,
@@ -123,5 +133,14 @@ class RandomGraphGenerator(object):
         temp = {}
         for i in range(len(set)):
             v1, v2, cost = set.pop()
-            temp['v{}'.format(i)] = [v1, v2, cost]
+            temp.setdefault(i, [v1, v2, cost])
+        return temp
+
+    @timeit
+    def convertSetToOrderedDict(self, set):
+        temp = {}
+        set = sorted(set)
+        for i in range(len(set)):
+            v1, v2, cost = set.pop()
+            temp.setdefault(i, [v1, v2, cost])
         return temp
