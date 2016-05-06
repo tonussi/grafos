6791cc7b842d7dd956c74091bb0078bc584db4a2
tonussi <lptonussi@gmail.com>
2016-05-06 20:08:52 -0300

    Add tests against 2kk random elements odd index and even index

--- HeapsortTest.py
+++ HeapsortTest.py
@@ -1,18 +1,42 @@
-import unittest
+import unittest, random
 from HeapSort import HeapSort
+from test.support import bigaddrspacetest
 
 class GraphTest(unittest.TestCase):
 
     def setUp(self):
         self.heap = HeapSort()
-        self.array = [8, 99, 12, 839, 12, 32, 89, 11, 99, 4, 12, 2, 13]
+        self.oddarray = [8, 99, 12, 839, 12, 32, 89, 11, 99, 4, 12, 2, 13]
+        self.evenarray = [8, 99, 12, 839, 12, 32, 89, 11, 99, 4, 2, 13]
 
     def tearDown(self):
         del self.heap
 
-    def testSortingWithHeapSort(self):
-        self.heap.heapsort(self.array, len(self.array))
-        print(self.array)
+    """
+    Testing against 13 random elements is passing (odd index)
+    """
+    def testSortingWithHeapSortOddIndex(self):
+        self.heap.heapsort(self.oddarray, len(self.oddarray))
+        for i in range(1, len(self.oddarray) - 1):
+            self.assertTrue(self.oddarray[i] <= self.oddarray[i + 1], 'oddarray[{}] should be leq than oddarray[{}]'.format(i, i + 1))
+
+    """
+    Testing against 12 random elements is passing (odd index)
+    """
+    def testSortingWithHeapSortEvenIndex(self):
+        self.heap.heapsort(self.evenarray, len(self.evenarray))
+        for i in range(1, len(self.evenarray) - 1):
+            self.assertTrue(self.evenarray[i] <= self.evenarray[i + 1], 'oddarray[{}] should be leq than oddarray[{}]'.format(i, i + 1))
+
+
+    """
+    Testing against 2kk random elements is passing
+    """
+    def testAgainstBigArray(self):
+        bigarray = [random.random() for _ in range(2000000)]
+        self.heap.heapsort(bigarray, len(bigarray))
+        for i in range(1, len(bigarray) - 1):
+            self.assertTrue(bigarray[i] <= bigarray[i + 1], 'bigarray[{}]={} should be leq than bigarray[{}]={}'.format(i, bigarray[i], i + 1, bigarray[i + 1]))
  
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
