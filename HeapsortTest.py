a31884654c1458380a7b0a043cbc0c429a047fd6
tonussi <lptonussi@gmail.com>
2016-05-06 19:51:17 -0300

    Add tests for HeapSort class

--- HeapsortTest.py
+++ HeapsortTest.py
@@ -1,17 +1,19 @@
 import unittest
-from Heapsort import Heapsort
+from HeapSort import HeapSort
 
 class GraphTest(unittest.TestCase):
 
     def setUp(self):
-        self.heap = Heapsort()
+        self.heap = HeapSort()
+        self.array = [8, 99, 12, 839, 12, 32, 89, 11, 99, 4, 12, 2, 13]
 
     def tearDown(self):
         del self.heap
 
     def testSortingWithHeapSort(self):
-        pass
-        
+        self.heap.heapsort(self.array, len(self.array))
+        print(self.array)
+ 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
