32afd1b41b03df4a8f25a49dec0f2137d38cc624
tonussi <lptonussi@gmail.com>
2016-05-06 19:50:46 -0300

    Encapsulate

--- HeapSort.py
+++ HeapSort.py
@@ -1,28 +1,23 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+class HeapSort:
 
-def heapsort(a, n):
-    for i in range((n/2), -1, -1):                
-        ajuste(a, i, n)
-    for i in range((n-1), -1, -1):                
-        temp = a[i]
-        a[i] = a[0]
-        a[0] = temp
-        ajuste(a,0,i);
-		
-def ajuste(a, i, n):
-	aAux = a[i]
-	j = 2*i
-	while j < n:
-		if (j < (n - 1) and a[j] < a[j+1]):
-			j = j + 1
-		if (aAux >= a[j]):
-			return		
-		a[j/2] = a[j]
-		a[j] = aAux
-		j = 2*j
-	a[j/2] = aAux
-	
-a = [4, 1, 6, 7, 3, 4, 0]
-heapsort(a, len(a))
-print(a)
+    def heapsort(self, a, n):
+        for i in range((n // 2), -1, -1):
+            self.ajuste(a, i, n)
+        for i in range((n - 1), -1, -1):
+            temp = a[i]
+            a[i] = a[0]
+            a[0] = temp
+            self.ajuste(a, 0, i)
+
+    def ajuste(self, a, i, n):
+        aAux = a[i]
+        j = 2 * i
+        while j < n:
+            if (j < (n - 1) and a[j] < a[j + 1]):
+                j = j + 1
+            if (aAux >= a[j]):
+                return
+            a[j // 2] = a[j]
+            a[j] = aAux
+            j = 2 * j
+        a[j // 2] = aAux
