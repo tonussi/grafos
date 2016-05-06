903b47296de36967222d73b54d8f241b1dec12d0
tonussi <lptonussi@gmail.com>
2016-05-06 13:12:00 -0300

    Emplace inside a class called Dijsktra

--- Dijkstra.py
+++ Dijkstra.py
@@ -2,21 +2,31 @@ from Graph import Graph
 
 INFINITE = 99999
 
-def Dijkstra(graph, distancesFromS, s):
-	distances = {}
-	distances[s] = 0
-	for v in distancesFromS.keys:
-		if (v != s):
-			distances[v] = INFINITE
-	orderedVertexes = []
-	q = distancesFromS
-	while len(q) != 0:
-		u = min(q, key=q.get)
-		del q[u]
-		orderedVertexes.append(u)
-		for v in graph.vertexAdjacencies(u).keys:
-			if distances[v] > distances[u] + graph.vertexAdjacencies(u)[v]:
-				distances[v] = distances[u] + graph.vertexAdjacencies(u)[v]
-	return distances
-		
-		
+class Dijkstra:
+
+	"""
+	This is the dijkstra method to find the shortest
+	path
+
+	@param graph: a graph of type Graph
+	@param distancesFromS: ?
+	@param s: ?
+	"""
+	def dijkstra(self, graph, distancesFromS, s):
+		distances = {}
+		distances[s] = 0
+
+		for v in distancesFromS.keys:
+			if (v != s):
+				distances[v] = INFINITE
+
+		orderedVertexes = []
+		q = distancesFromS
+		while len(q) != 0:
+			u = min(q, key=q.get)
+			del q[u]
+			orderedVertexes.append(u)
+			for v in graph.vertexAdjacencies(u).keys:
+				if distances[v] > distances[u] + graph.vertexAdjacencies(u)[v]:
+					distances[v] = distances[u] + graph.vertexAdjacencies(u)[v]
+		return distances
