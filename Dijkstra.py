d76e7d97551067bdd0b21170e0aad611164a8949
tonussi <lptonussi@gmail.com>
2016-05-09 00:36:35 -0300

    Add dijkstra

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,22 +1,25 @@
 from Graph import Graph
+from HeapSort import HeapSort
 
 INFINITE = 99999
 
 class Dijkstra:
 
 	"""
-	This is the dijkstra method to find the shortest
-	path
-
+	This is the dijkstra method to find the shortest path
 	@param graph: a graph of type Graph
-	@param distancesFromS: ?
-	@param s: ?
+	@param distancesFromS: all adjacencies of the starting node
+	@param s: target node
 	"""
 	def dijkstra(self, graph, distancesFromS, s):
+
+		if s not in graph.graph:
+			raise
+
 		distances = {}
 		distances[s] = 0
 
-		for v in distancesFromS.keys:
+		for v in distancesFromS.adjacencies.keys:
 			if (v != s):
 				distances[v] = INFINITE
 
