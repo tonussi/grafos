9e84fe75dec08119999a7dccc1b4934d141f3424
tonussi <lptonussi@gmail.com>
2016-05-25 01:21:16 -0300

    Add dijkstra for testing purposes

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,14 +1,16 @@
-from Graph import Graph
-from HeapSort import HeapSort
 from Chronometer import timeit
 from PriorityDict import PriorityDict
+
+from heapq import heappop, heappush
 
 INFINITE = 99999
 
 class Dijkstra:
 
     """
-    Find shortest paths from the  start vertex to all vertices nearer than or equal to the end.
+    @summary: Find shortest paths from the  start vertex to all
+              vertices nearer than or equal to the end.
+    @see: https://www.youtube.com/watch?v=CHvQ3q_gJ7E
 
     @param graph: a graph of type Graph
     @param distancesFromS: all adjacencies of the starting node
@@ -17,9 +19,9 @@ class Dijkstra:
     @timeit
     def dijkstra(self, G, start, end=None):
 
-        D = {}  # dictionary of final distances
-        P = {}  # dictionary of predecessors
-        Q = PriorityDict()  # estimated distances of non-final vertices
+        D = {} # dictionary of final distances
+        P = {} # dictionary of predecessors
+        Q = PriorityDict() # estimated distances of non-final vertices
         Q[start] = 0 # add zero cost
 
         for v in Q:
@@ -39,3 +41,25 @@ class Dijkstra:
                     P[w] = v
 
         return D, P
+
+    """
+    Find shortest paths from the  start vertex to all vertices nearer than or equal to the end.
+
+    @param graph: a graph of type Graph
+    @param distancesFromS: all adjacencies of the starting node
+    @param s: target node
+    """
+    @timeit
+    def dijkstraWithBuiltinHeap(self, G, start, end=None):
+        D = {} # dictionary of final distances
+        P = {} # dictionary of predecessors
+        Q = [(0, None, start)] # heap of (est.dist., pred., vert.)
+        while Q:
+            dist, pred, v = heappop(Q)
+            if v in D:
+                continue # tuple outdated by decrease-key, ignore
+            D[v] = dist
+            P[v] = pred
+            for w in G.getVertex(v).adjacencies:
+                heappush(Q, (D[v] + G.vertexAdjacencies(v).get(w).getcost(), v, w))
+        return (D,P)
